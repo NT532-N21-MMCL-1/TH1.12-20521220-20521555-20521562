ac3bdd6df93ef03a559f1a11b73091c3e06b80c3
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-04-18 16:20:40 +0700

    task7 final code

--- Lab 2/Task7/Task7.ino
+++ Lab 2/Task7/Task7.ino
@@ -1,55 +1,56 @@
+#include "OneButton.h"
+
 int sensorPin = A0;
-int sensorValue=0;
+int sensorValue = 0;
+int buttonPin = A1;
 
-int ledPin[] = {13,12,11,10,9,8,7,6,5,4};
+int ledsPin[] = {4,5,6,7,8,9,10,11,12,13};
+int ledsNumber;
 
-int buttonPin = 3;
-int preStateButton = LOW;
-int count = 0;
-void setup()
-{
+OneButton button(buttonPin, true);
+bool stop=false;
+
+void setup() {
   Serial.begin(9600);
   pinMode(sensorPin, INPUT);
-  for(int i=0; i<sizeof(ledPin); i++){
-  	pinMode(ledPin[i], OUTPUT);
+  for (int i=0; i<sizeof(ledsPin); i++){
+    pinMode(ledsPin[i], OUTPUT);
   }
-  pinMode(buttonPin, INPUT);
+  button.attachClick(click_button);
+  button.attachDoubleClick(doubleclick_button);
 }
 
-void loop()
-{
-  sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
-  delay(500);
-  for(int i=0; i<sizeof(ledPin); i++){
-  	digitalWrite(ledPin[i], LOW);
-  }
-  int ledsDisplay = map(sensorValue,679,6,0,sizeof(ledPin));
-  for(int i=0; i<ledsDisplay; i++){
-    digitalWrite(ledPin[i], HIGH);
-  }
-  
-  int curStateButton = digitalRead(buttonPin);
-  if (curStateButton==HIGH && preStateButton==LOW){
-    count++;
-    Serial.println(count);
-  }
-  preStateButton=curStateButton;
-  
-  if (ledsDisplay==6){
-  	if(count==1){
-    	Serial.println("Led mode: 1");
-    	for(int i=0; i<5; i++){
-      	digitalWrite(ledPin[i], HIGH);
-  		} 
-  	}
-  
-  	if(count==2){
-    	Serial.println("Led mode: 2");
-    	for(int i=0; i<10; i++){
-      	digitalWrite(ledPin[i], HIGH); 
-    	}
-  	}
-  }
-  delay(300);
+void loop() {
+    sensorValue = analogRead(sensorPin);
+    Serial.println(sensorValue);
+    button.tick();
+    delay(10);
+
+    for(int i=0; i<10; i++){
+      digitalWrite(ledsPin[i], LOW);
+    }
+
+    if(stop){
+      ledsNumber = map(sensorValue, 0, 1023, 0, 5);
+    }
+
+    if(!stop){
+      ledsNumber = map(sensorValue, 0, 1023, 0, 10);
+    }
+
+
+    for(int i=0; i<ledsNumber; i++){
+      digitalWrite(ledsPin[i], HIGH);
+    }
+}
+
+
+void click_button(){
+  stop = true;
+  Serial.println("1 click");
+}
+
+void doubleclick_button(){
+  stop = false;
+  Serial.println("double click");
 }
