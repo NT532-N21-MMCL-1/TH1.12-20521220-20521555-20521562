41f96058d054d8eb29b6ddd93ae5a7d2461630be
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-15 10:08:12 +0700

    update lab3/task2

--- Lab 3/Task2/FastAPI/main.py
+++ Lab 3/Task2/FastAPI/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
+class sensorValue(BaseModel):
+    light: float
+    distance: int
 
-@app.get('/')
-async def root():
-    return {"message": "hello world"}
+class Body(BaseModel):
+    data: sensorValue
+
+class Response(BaseModel):
+    ledsNum: int
+
+@app.post('/')
+async def root(body:Body):
+    num = 0
+    lux = body.data.light
+    distance = body.data.distance
+
+    if distance<5:
+        if 0<lux<80: num = 3
+        elif 80<lux<160: num = 2
+        elif 160<lux<240: num = 1
+        else: ledsNum = 0
+
+    response = Response(ledsNum = num)
+
+    response_jsonTostr = jsonable_encoder(response)
+    return response_jsonTostr
--- Lab 3/Task2/Task2.ino
+++ Lab 3/Task2/Task2.ino
@@ -2,10 +2,34 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
+#include <NewPing.h>
+#include <BH1750.h>
+#include <Wire.h>
+
+#include <ArduinoJson.h>
+
+int ledsPin[] = {D5, D6, D7};
+
+#define trigPin D6
+#define echoPin D7
+
+BH1750 lightSensor;
+
+NewPing distSensor(trigPin, echoPin, 10);
+long distance, duration;
+
 const char* ssid = "UiTiOt-E3.1";
 const char* password = "UiTiOtAP";
 const char* url = "http://172.31.11.55:8000/";
 const int port = 8000;
+
+HTTPClient http;
+WiFiClient wifiClient;
+
+StaticJsonDocument<200> jsonObj;
+StaticJsonDocument<200> response;
+
+JsonObject sensorData = jsonObj.createNestedObject("data");
 
 void setup() {
   Serial.begin(9600);
@@ -15,30 +39,61 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  for (int i=0; i<sizeof(ledsPin); i++){
+    pinMode(ledsPin[i], OUTPUT);
+  }
+
+  Wire.begin();
+  lightSensor.begin();
 
   Serial.println("Connected to WiFi");
 
-  // Gọi API
-  HTTPClient http;
-  WiFiClient wifiClient;
-
   http.begin(wifiClient, url);
   //http.addHeader("Content-Type", "application/json");
+}
 
-  int httpResponseCode = http.GET();
+void loop() {
+  for (int i=0; i<sizeof(ledsPin); i++){
+    digitalWrite(ledsPin[i], LOW);
+  }
+
+  distance = distSensor.ping_cm();
+
+  Serial.print("distance: ");
+  Serial.println(distance);
+
+  float lightValue = lightSensor.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lightValue);
+  Serial.println(" lx");
+
+  sensorData["light"] = lightValue;
+  sensorData["distance"] = distance; 
+
+  String jsonString;
+  serializeJson(jsonObj, jsonString);
+  Serial.println(jsonString);
+
+  int httpResponseCode = http.POST(jsonString);
 
   if (httpResponseCode > 0) {
-    String response = http.getString();
     Serial.println(httpResponseCode);
-    Serial.println(response);
+    String strResponse = http.getString();
+    Serial.println(strResponse);
+
+    deserializeJson(response, strResponse);
+    int ledsNum = response["ledsNum"];
+    for (int i=0; i<ledsNum; i++){
+      digitalWrite(ledsNum, HIGH);
+    }
+
   } else {
     Serial.print("Error on HTTP request: ");
     Serial.println(httpResponseCode);
   }
-
   http.end();
-}
-
-void loop() {
-  // do nothing
+  delay(5000);
 }
