7b764e3dfdc74128c4bd53078c13803c0e9dc4dd
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-18 06:12:06 +0700

    update HomeScreen

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/ApiService.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/ApiService.java
@@ -2,8 +2,9 @@ package com.example.lab4_iot;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface ApiService {
-    @GET("http://127.0.0.1:8000/")
-    Call<stateWeMos> getWeMosState(String name);
+    @GET("wemos/state/{name}")
+    Call<stateWeMos> getWeMosState(@Path("name") String nameWeMos);
 }
--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/HomeFragment.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/HomeFragment.java
@@ -4,15 +4,39 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ApiService apiService = ApiClient.getApiService();
+        Call<stateWeMos> stateWeMosCall = apiService.getWeMosState("d1A");
+        stateWeMosCall.enqueue(new Callback<stateWeMos>() {
+            @Override
+            public void onResponse(Call<stateWeMos> call, Response<stateWeMos> response) {
+                stateWeMos state = response.body();
+                Log.d("wemos info", String.valueOf(response.code()));
+                Log.d("wemos info", state.toString());
+                //Toast.makeText(getActivity(),state.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<stateWeMos> call, Throwable t) {
+                Log.d("wemos info", t.toString());
+            }
+        });
+
+        return view;
     }
 }
