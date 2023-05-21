5481f04aa047ec58bd835dc600df48f414098967
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-21 09:59:22 +0700

    test websocket

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/HomeFragment.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/HomeFragment.java
@@ -15,12 +15,20 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
     ApiService apiService = ApiClient.getApiService();
+    private WebSocket webSocket;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,10 +38,41 @@ public class HomeFragment extends Fragment {
         txtStateA = view.findViewById(R.id.txtStateA);
         txtStateB = view.findViewById(R.id.txtStateB);
 
-        callStateAPI("d1A", txtStateA);
-        callStateAPI("d1B", txtStateB);
+//        callStateAPI("d1A", txtStateA);
+//        callStateAPI("d1B", txtStateB);
 
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request
+                .Builder().url("ws://192.168.1.25:8080").build();
+        webSocket = client.newWebSocket(request, new MyWebSocketListener());
         return view;
+    }
+
+    private class MyWebSocketListener extends WebSocketListener{
+        @Override
+        public void onOpen(WebSocket webSocket, okhttp3.Response response){
+            super.onOpen(webSocket, response);
+            Log.d("WebSocket", "Connected");
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text){
+            super.onMessage(webSocket, text);
+            Log.d("WebSocket", "Received message: " + text);
+        }
+
+        @Override
+        public void onClosed(WebSocket webSocket,int code, String reason){
+            super.onClosed(webSocket, code, reason);
+            Log.d("WebSocket", "Disconnected");
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, okhttp3.Response response) {
+            super.onFailure(webSocket, t, response);
+            // Xử lý khi gặp lỗi kết nối WebSocket
+            Log.e("WebSocket", "Error: " + t.getMessage());
+        }
     }
 
     private void callStateAPI(String name, TextView txtState){
