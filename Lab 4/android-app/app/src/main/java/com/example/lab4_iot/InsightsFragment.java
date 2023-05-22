6795d8576070e62f14618bf16807c92c0c9bb3cb
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-22 21:49:17 +0700

    test mpandroid chart

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/InsightsFragment.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/InsightsFragment.java
@@ -1,5 +1,6 @@
 package com.example.lab4_iot;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,67 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 public class InsightsFragment extends Fragment {
-
+    String[] item = {"Temperature", "Humidity", "Light"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+    private LineChart lineChart;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_insights, container, false);
+        View view = inflater.inflate(R.layout.fragment_insights, container, false);
+        lineChart = view.findViewById(R.id.lineChart);
+        lineChart.setDrawGridBackground(false);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 4));
+        entries.add(new Entry(1, 6));
+        entries.add(new Entry(2, 2));
+        entries.add(new Entry(3, 8));
+        entries.add(new Entry(4, 5));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Data");
+        dataSet.setColor(Color.parseColor("#007dfe"));
+        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setLineWidth(3f);
+
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+
+        XAxis xAxis = lineChart.getXAxis();
+        YAxis yAxisLeft = lineChart.getAxisLeft();
+        YAxis yAxisRight = lineChart.getAxisRight();
+
+        xAxis.setDrawGridLines(false);
+
+        lineChart.invalidate();
+
+        autoCompleteTextView = view.findViewById(R.id.auto_complete_txt);
+        adapterItems = new ArrayAdapter<String>(getContext(), R.layout.list_chart_select, item);
+
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getContext(), "Item: " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
