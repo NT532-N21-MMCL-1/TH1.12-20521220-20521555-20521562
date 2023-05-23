d4a7acd76e3c0c798595d332b5b35341c9611b24
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-23 11:45:08 +0700

    add screen title bar

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/MainActivity.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/MainActivity.java
@@ -7,15 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView titleScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleScreen = findViewById(R.id.titleScreen);
+        titleScreen.setText("Welcome");
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
@@ -30,15 +34,19 @@ public class MainActivity extends AppCompatActivity {
                 switch(item.getItemId()) {
                     case R.id.navigation_home:
                         selectedFragment = new HomeFragment();
+                        titleScreen.setText("Welcome");
                         break;
                     case R.id.navigation_dashboard:
                         selectedFragment = new DashboardFragment();
+                        titleScreen.setText("Dashboard & Control Lights");
                         break;
                     case R.id.navigation_insights:
                         selectedFragment = new InsightsFragment();
+                        titleScreen.setText("Visualize Data");
                         break;
                     case R.id.navigation_logs:
                         selectedFragment = new LogsFragment();
+                        titleScreen.setText("System Logs");
                         break;
                 }
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
