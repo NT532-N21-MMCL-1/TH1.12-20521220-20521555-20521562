208f687bb30c3f08745b1ff31ebed6c346b9c275
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-25 23:04:50 +0700

    add mqtt information

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/DashboardFragment.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/DashboardFragment.java
@@ -8,6 +8,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -15,6 +17,13 @@ public class DashboardFragment extends Fragment {
     public DashboardFragment() {
 
     }
+
+    private String TAG = "mqtt check";
+    private ImageButton light1, light2;
+
+    private static final String BROKER_URL = "tcp://172.31.9.11:1883";
+    private static final String CLIENT_ID = "client_android";
+    private MqttHandler mqttHandler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +33,9 @@ public class DashboardFragment extends Fragment {
         ViewPager viewPager = view.findViewById(R.id.view_pager);
         TabLayout tabLayout = view.findViewById(R.id.tab_layout);
 
+        light1 = view.findViewById(R.id.btnLight1);
+        light2 = view.findViewById(R.id.btnLight2);
+
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());
         adapter.addFragment(new TemperatureRoomFragment(), "Temperature");
         adapter.addFragment(new HumidityRoomFragment(), "Humidity");
@@ -32,6 +44,22 @@ public class DashboardFragment extends Fragment {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        mqttHandler = new MqttHandler();
+        mqttHandler.connect(BROKER_URL,CLIENT_ID);
+
+        light1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mqttHandler.publish("mmcl/nhom12/led/n1", "clicked");
+            }
+        });
+
+        light2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mqttHandler.publish("mmcl/nhom12/led/n2", "clicked");
+            }
+        });
         return view;
     }
 }
