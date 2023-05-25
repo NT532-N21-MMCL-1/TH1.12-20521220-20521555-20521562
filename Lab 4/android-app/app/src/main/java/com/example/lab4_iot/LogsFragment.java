19f8b5616c91b424ae5604311d607e6038b6d55b
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-25 23:03:03 +0700

    design logs screen

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/LogsFragment.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/LogsFragment.java
@@ -7,13 +7,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class LogsFragment extends Fragment {
+    private ListView listView;
+    private TextView logTemp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logs, container, false);
+        View view = inflater.inflate(R.layout.fragment_logs, container, false);
+        listView = view.findViewById(R.id.list_view);
+
+        // Tạo một mảng dữ liệu giả để hiển thị trong ListView
+        String[] data = {"Mục 1 - Text 1", "Mục 2 - Text 2", "Mục 3 - Text 3", "Mục 4 - Text 4", "Mục 5 - Text 5"};
+
+        // Tạo một ArrayAdapter để hiển thị dữ liệu trong ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.list_item, data) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                String item = getItem(position);
+                String[] parts = item.split(" - ");
+
+                TextView text1 = view.findViewById(R.id.logTemp);
+
+                text1.setText(parts[0]);
+
+                return view;
+            }
+        };
+
+        // Gắn adapter vào ListView
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
