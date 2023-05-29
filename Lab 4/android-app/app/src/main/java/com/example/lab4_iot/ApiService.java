badeea8e8fa77d1fe83e9f5556b682cf7be38f5e
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-05-29 08:23:22 +0700

    modify url

--- Lab 4/android-app/app/src/main/java/com/example/lab4_iot/ApiService.java
+++ Lab 4/android-app/app/src/main/java/com/example/lab4_iot/ApiService.java
@@ -1,5 +1,6 @@
 package com.example.lab4_iot;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -8,12 +9,12 @@ public interface ApiService {
     @GET("sqlite/retrieve/all_data")
     Call<ListSensorValue[]> getListSensorValue();
 
-    @GET("sqlite/retrieve/current_data")
-    Call<ListSensorValue> getCurrentSensorValue();
+    @GET("sqlite/retrieve/current_data/{column_name}")
+    Call<Float> getCurrentSensorValue(@Path("column_name") String name);
 
     @GET("sqlite/retrieve/column_size/{column_name}")
     Call<Integer> getColumnSize(@Path("column_name") String name);
 
-    @GET("sqlite/retrieve/amount_of_data/{num}")
-    Call<ListSensorValue[]> getNumOfData(@Path("num") String num);
+    @GET("sqlite/retrieve/amount_of_data/{column_name}/{num}")
+    Call<ResponseBody> getNumOfData(@Path("column_name") String name, @Path("num") String num);
 }
